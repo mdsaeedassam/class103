140bfd0b0b31f87155f921238dfa8852450cb90e
mdsaeedassam <128061748+mdsaeedassam@users.noreply.github.com>
2023-07-16 18:26:12 +0530

    Add files via upload

---
+++ chat_room.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBC2dGXx0Ik3qBtJsAcgviLMJRJAoytB4Q",
+    authDomain: "kwitter-project-bdf5c.firebaseapp.com",
+    databaseURL: "https://kwitter-project-bdf5c-default-rtdb.firebaseio.com",
+    projectId: "kwitter-project-bdf5c",
+    storageBucket: "kwitter-project-bdf5c.appspot.com",
+    messagingSenderId: "233675170808",
+    appId: "1:233675170808:web:b2d1a5df75caf5699dec0d"
+  };
+  
